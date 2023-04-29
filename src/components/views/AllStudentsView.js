3fac52fdc063ce807632d2fdb134fa62c70aefc5
Abrar <amahi2001@gmail.com>
2023-04-29 19:48:29 -0400

    new UI for all students and all campus view

--- src/components/views/AllStudentsView.js
+++ src/components/views/AllStudentsView.js
@@ -5,47 +5,64 @@ The Views component is responsible for rendering web page with data provided by 
 It constructs a React component to display the all students view page.
 ================================================== */
 import { Link } from "react-router-dom";
+//react bootstrap
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from 'react-bootstrap/Button';
 
-const AllStudentsView = (props) => {
-  const {students, deleteStudent} = props;
+export default function AllStudentsView(props) {
+  const { students, deleteStudent } = props;
+
+  const AddStudent = () => (
+    <Button as={Link} to={`/newstudent/`} variant='outline-dark' className="my-2">
+      + Add Student
+    </Button>
+  )
   // If there is no student, display a message
   if (!students.length) {
     return (
-    <div>
-      <p>There are no students.</p>
-      <Link to={`newstudent`}>
-        <button>Add New Student</button>
-      </Link>
-    </div>
+      <Container>
+        <h1 className='text-center my-4 display-6'>There are No Students</h1>
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <AddStudent />
+          </Col>
+        </Row>
+      </Container>
     );
   }
-  
+
   // If there is at least one student, render All Students view 
   return (
-    <div>
-      <h1>All Students</h1>
+    <Container>
+      <h1 className='text-center my-4 display-6'>All Students</h1>
+      <AddStudent />
 
-      {students.map((student) => {
-          let name = student.firstname + " " + student.lastname;
-          return (
-            <div key={student.id}>
-              <Link to={`/student/${student.id}`}>
-                <h2>{name}</h2>
-              </Link>
-              <button onClick={() => deleteStudent(student.id)}>Delete</button>
-              <hr/>
-            </div>
-          );
-        }
-      )}
-      <br/>
-      <Link to={`/newstudent`}>
-        <button>Add New Student</button>
-      </Link>
-      <br/><br/>
-    </div>
+      <ListGroup as="ol">
+        {students.map((student) => (
+          <ListGroup.Item
+            as="li"
+            className="d-flex justify-content-between align-items-start"
+            key={student.id}
+          >
+            <span className="ms-2 me-auto">
+              <div className="fw-bold">{student.firstname + " " + student.lastname}</div>
+            </span>
+            <ButtonGroup aria-label="Basic example">
+              <Button as={Link} to={`/student/${student.id}`} variant='outline-dark'>
+                {`ID: ${student.id}`}
+              </Button>
+              <Button variant='outline-danger' onClick={() => deleteStudent(student.id)}>
+                Delete
+              </Button>
+            </ButtonGroup>
+          </ListGroup.Item>
+
+        ))}
+      </ListGroup>
+    </Container>
   );
-};
-
-
-export default AllStudentsView;
+}
