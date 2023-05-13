aec35ef401990b5d750d8ddb1f585e4cbc44a5f1
Abrar <amahi2001@gmail.com>
2023-05-13 17:50:07 -0400

    image URL field + adding imageURL for campus in newCampus

--- src/components/containers/NewCampusContainer.js
+++ src/components/containers/NewCampusContainer.js
@@ -21,6 +21,7 @@ class NewCampusContainer extends Component {
       name: "",
       address: "",
       description: "",
+      imageURL: "",
       redirect: false,
       redirectId: null
     };
@@ -40,7 +41,8 @@ class NewCampusContainer extends Component {
     let campus = {
       name: this.state.campusName,
       address: this.state.address,
-      description: this.state.description
+      description: this.state.description,
+      imageURL: this.state.imageURL
     };
 
     // Add new student in back-end database
@@ -51,6 +53,7 @@ class NewCampusContainer extends Component {
       name: "",
       address: "",
       description: "",
+      imageURL: "",
       redirect: true,
       redirectId: newCampus.id
     });
--- src/components/views/NewCampusView.js
+++ src/components/views/NewCampusView.js
@@ -1,36 +1,68 @@
-import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export default function NewCampusView(props) {
-    const { handleChange, handleSubmit } = props;
+  const { handleChange, handleSubmit } = props;
 
-    // Render a New Student view with an input form
-    return (
-        <Container>
-            <h1 className='text-center my-4 display-5'>Add New Campus</h1>
-            <Card className='p-2'>
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group className='mb-3' controlId='c_name'>
-                        <Form.Label>Campus Name</Form.Label>
-                        <Form.Control name="campusName" required placeholder='Example University' type="text" onChange={handleChange} />
-                    </Form.Group>
+  // Render a New Student view with an input form
+  return (
+    <Container>
+      <h1 className="text-center my-4 display-5">Add New Campus</h1>
+      <Card className="p-2">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="c_name">
+            <Form.Label>Campus Name</Form.Label>
+            <Form.Control
+              name="campusName"
+              required
+              placeholder="Example University"
+              type="text"
+              onChange={handleChange}
+            />
+          </Form.Group>
 
-                    <Form.Group className='mb-3' controlId=''>
-                        <Form.Label>Address</Form.Label>
-                        <Form.Control name="address" required placeholder='1234 Main St' type="text" onChange={handleChange} />
-                    </Form.Group>
+          <Form.Group className="mb-3" controlId="">
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              name="address"
+              required
+              placeholder="1234 Main St"
+              type="text"
+              onChange={handleChange}
+            />
+          </Form.Group>
 
-                    <Form.Group className='mb-3'>
-                        <Form.Label>Description</Form.Label>
-                        <Form.Control name="description" required placeholder='Enter a description' type="text" onChange={handleChange} />
-                    </Form.Group>
-                    <Button type="submit">
-                        Add Campus
-                    </Button>
-                </Form>
-            </Card>
-        </Container>
-    );
+          <Form.Group className="mb-3">
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              name="description"
+              required
+              placeholder="Enter a description"
+              type="text"
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Image URL</Form.Label>
+            <Form.Control
+              name="imageURL"
+              required
+              placeholder="https://example.com/image.jpg"
+              type="text"
+              onChange={handleChange}
+              pattern="https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
+            />
+            <Form.Text muted>
+                Please enter a valid URL starting with http:// or https://
+            </Form.Text>
+          </Form.Group>
+
+          <Button type="submit">Add Campus</Button>
+        </Form>
+      </Card>
+    </Container>
+  );
 }
