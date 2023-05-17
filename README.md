40d418628a0d5ddf89b9ee2efe85a728ec0205b4
Abrar <amahi2001@gmail.com>
2023-05-17 19:47:17 -0400

    new home page and readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# CSCI 39548: Web Development
+## Final Project: Campus Manangement System. Client Side
+### username: amahi2001
+<br/>
+The primary objective of this application is to offer a robust frontend using React and Redux, enabling seamless interactions with a campus management system. Users can effortlessly perform CRUD operations related to students and campuses by querying an Express server. It's worth noting that each campus is associated with multiple students, forming a one-to-many relationship. Furthermore, the backend efficiently writes data to a PostgreSQL database, ensuring reliability and data integrity.
+
+<br/>
+
+## Note
+The project expects the server to be running on port 8080. If you wish to change the port, you must do so manually on the server side as well as the client side.
+
+<br/>
+
+## Installation (client side)
+1. Clone the repository
+2. Run `npm install` to install the dependencies
+3. Run `npm run start` or `make run` to start the application
--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -4,6 +4,35 @@ HomePageView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the home page.
 ================================================== */
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
 export default function HomePageView() {
-  return <h1 className="text-center my-4 display-5">Home Page</h1>;
+  return (
+    <Container>
+      <h1 className="text-center my-4 display-5">Home Page</h1>;
+      <Row>
+        <h2 className="text-center my-4 display-5">
+          {" "}
+          Welcome to the campus management system
+        </h2>
+      </Row>
+      <Row md="auto" className="justify-content-md-center">
+        <Col>
+          <Button as={Link} to="/campuses" variant="outline-primary">
+            View Campuses
+          </Button>
+        </Col>
+        <Col>
+          {" "}
+          <Button as={Link} to="/students" variant="outline-primary">
+            View Students
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
