551b76c57c713ca2d54a7d723838ed1e904fa2c5
Abrar <amahi2001@gmail.com>
2023-05-17 00:12:37 -0400

    delete student reducer for single student view

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -10,16 +10,40 @@ import Container from "react-bootstrap/Container";
 import ListGroup from "react-bootstrap/ListGroup";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 // Take in props data to construct the component
 const CampusView = (props) => {
-  const { campus, deleteStudent } = props;
+  const { campus, deleteStudent, deleteCampus } = props;
   // Render a single Campus view with list of its students
   return (
     <Container>
-      <h1 className="text-center my-4 display-5">
-        {campus.id}. {campus.name}
-      </h1>
+      <Row md="auto" className="justify-content-md-center">
+        <Col>
+          <h1 className="text-center my-4 display-5">
+            {campus.id}. {campus.name}
+          </h1>
+        </Col>
+        {/* Edit and Delete Buttons */}
+        <Col className="d-flex align-items-center">
+          <ButtonGroup className="">
+            <Button
+              variant="outline-primary"
+              // onClick={() => history.push(`/edit-student/${student.id}`)}
+            >
+              Edit
+            </Button>
+            <Button
+              variant="outline-danger"
+              onClick={() => deleteCampus(campus.id)}
+            >
+              Delete
+            </Button>
+          </ButtonGroup>
+        </Col>
+      </Row>
+
       <Image
         className="d-block mx-auto my-4"
         alt="image unable to load"
